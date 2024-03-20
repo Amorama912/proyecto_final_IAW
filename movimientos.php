9070f34489fbf94cadd435483eee068c5eb8cd8c
Ale <amorama912a@ieslacampina.es>
2024-03-20 08:58:30 +0100

    crear base de movientos.php

--- movimientos.php
+++ movimientos.php
@@ -8,8 +8,63 @@
 
     //la ejecuto y guardo el resultado en $resultado
     $resultado = $mysqli->query($sql);
-
-    echo $id_cuenta;
-    
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+	
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js" ></script>
+	<script src="js/bootstrap.min.js" ></script>
+	<script src="js/jquery.dataTables.min.js" ></script>
+</head>
+<body>
+		<div class="container">
+			<div class="row">
+				<h1>Banco La Campiña</h1>
+			</div>
+			<br>
+			
+			<div class="row">
+				<!-- Registrar -->
+				<a href="añadircliente.php"><button type="button" class="btn btn-primary">Añadir cliente</button></a>
+			</div>
+			<br>
+			<br>
+			
+			<table id="tabla" class="display" style="width:100%">
+				<thead>
+					<tr>
+						<th>Cantidad</th>
+						<th>Tipo</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					//Insertar los datos en la tabla recorriendo el array
+						while($fila = $resultado->fetch_assoc()){
+							echo "<tr>";
+								echo "<td>$fila[cantidad]</td>";
+								echo "<td>$fila[tipo]</td>";
+								echo "<td><a href='eliminar.php?id=$fila[id_cliente]'><button type='button' class='btn btn-danger'>Eliminar cliente</button></td>";
+						}
+					?>
+				</tbody>
+			</table>
+			<?php
+				//cerramos la conexion con la base de datos
+				$mysqli->close();
+			?>
+		</div>
+</body>
+</html>
